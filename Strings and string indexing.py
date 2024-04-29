6ebfe8c3e013276bed5ae185a71bfa487771d19d
CyeToJIog <168110110+CyeToJIog@users.noreply.github.com>
2024-04-29 10:19:45 +0300

    Add files via upload

---
+++ Strings and string indexing.py
@@ -0,0 +1,8 @@
+name = "это моя строка "
+name1 = "это новая строка"
+print(name * 6)    #1
+print(name[-2])    #2
+print(name[0:4])   #3
+print(name[::-1])  #4
+print(len(name))   #5
+print(name1, name) #6
