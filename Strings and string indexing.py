d997b4612d39dc4e9d62adb7e8eff87b867d68af
CyeToJIog <168110110+CyeToJIog@users.noreply.github.com>
2024-04-30 12:48:14 +0300

    Update Strings and string indexing.py

--- Strings and string indexing.py
+++ Strings and string indexing.py
@@ -1,8 +1,8 @@
-name = "это моя строка "
+name = "это моя строка"
 name1 = "это новая строка"
-print(name * 6)    #1
-print(name[-2])    #2
-print(name[0:4])   #3
+print(name[0])    #1
+print(name[-1])    #2
+print(name[2:5])   #3
 print(name[::-1])  #4
 print(len(name))   #5
-print(name1, name) #6
+print(name1 + name) #6
